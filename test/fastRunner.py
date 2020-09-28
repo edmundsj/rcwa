f397f385446d1e1e58c8aa460d8590aca036ace1
edmundsj <jordan.e@berkeley.edu>
2020-09-28 10:27:12 -0700

    deliberately introducing bug to see if test failure causes commit failure'

--- test/fastRunner.py
+++ test/fastRunner.py
@@ -5,15 +5,15 @@ import unittest
 import sys
 sys.path.append('source')
 
-import testCrystal
-import testHarmonics
-import testLayer
-import testMaterial
-import testMatrices1x1Harmonics
-import testMatrices3x3Harmonics
-import testNetlistParser
-import testSolver1x1Harmonics
-import testSource
+import test.testCrystal as testCrystal
+import test.testHarmonics as testHarmonics
+import test.testLayer as testLayer
+import test.testMaterial as testMaterial
+import test.testMatrices1x1Harmonics as testMatrices1x1Harmonics
+import test.testMatrices3x3Harmonics as testMatrices3x3Harmonics
+import test.testNetlistParser as testNetlistParser
+import test.testSolver1x1Harmonics as testSolver1x1Harmonics
+import test.testSource as testSource
 
 loader = unittest.TestLoader()
 suite = unittest.TestSuite()
