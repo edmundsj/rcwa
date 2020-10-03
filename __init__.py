f6bd7da84a16bc181cf54218bf6315e6985d1409
edmundsj <jordan.e@berkeley.edu>
2020-10-03 09:10:06 -0700

    added Source, layer, solver, and plotter to __init__.py to get examples to work

--- __init__.py
+++ __init__.py
@@ -1,3 +1,6 @@
 import context
 import source
 from layer import LayerStack, Layer
+from source import Source
+from solver import RCWASolver as Solver
+from plotter import Plotter
--- examples/bragg_5layer.py
+++ examples/bragg_5layer.py
@@ -8,27 +8,21 @@ import RCWA
 import numpy as np
 import scipy as sp
 import scipy.linalg
-sys.path.append('../core/')
-#sys.path.append('../')
-from layer import Layer, LayerStack
-from source import Source
-from plotter import Plotter
 from matplotlib import pyplot as plt
 #from RCWA import RCWASolver # this is the syntax I want
-from solver import RCWASolver # This is the syntax I currently have
 
 lambda0 = 1.3
 nSi = 3.5
 nSiO2 = 1.45
 tSi = lambda0/4/nSi
 tSiO2 = lambda0/4/nSiO2
-layer0 = Layer(n=nSi)
-layer1 = Layer(n=nSiO2, L=tSiO2)
-layer2 = Layer(n=nSi, L=tSi)
-layer3 = Layer(n=nSiO2, L=tSiO2)
-layer4 = Layer(n=nSi, L=tSi)
-layer5 = Layer(n=nSiO2, L=tSiO2)
-layer6 = Layer(n=1)
+layer0 = RCWA.Layer(n=nSi)
+layer1 = RCWA.Layer(n=nSiO2, L=tSiO2)
+layer2 = RCWA.Layer(n=nSi, L=tSi)
+layer3 = RCWA.Layer(n=nSiO2, L=tSiO2)
+layer4 = RCWA.Layer(n=nSi, L=tSi)
+layer5 = RCWA.Layer(n=nSiO2, L=tSiO2)
+layer6 = RCWA.Layer(n=1)
 stack = RCWA.LayerStack(layer0, layer1, layer2, layer3, layer4, layer5, layer6)
 source = RCWA.Source(wavelength=0.4)
 
@@ -37,11 +31,11 @@ stopWavelength = 1.5
 stepWavelength = 0.002
 
 print("Solving system...")
-TMMSolver1 = RCWASolver(stack, source, (1, 1))
+TMMSolver = RCWA.Solver(stack, source, (1, 1))
 wavelengths = np.arange(startWavelength, stopWavelength + stepWavelength,
         stepWavelength)
 
-TMMSolver1.Solve(wavelengths=wavelengths)
+TMMSolver.Solve(wavelengths=wavelengths)
 #Plotter.plotEllipsometrySpectra(TMMSolver1.results)
-Plotter.plotRTEMSpectra(TMMSolver1.results)
+RCWA.Plotter.plotRTEMSpectra(TMMSolver.results)
 plt.show()
