ec5231cdba4f609e8acd519adc76ef8c8b832048
edmundsj <jordan.e@berkeley.edu>
2020-10-03 11:18:54 -0700

    added LICENSE.txt, fixed names / version in setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="RCWATest", # Replace with your own username
+    version="0.0.1",
+    author="Jordan Edmunds",
+    author_email="jordan.e@berkeley.edu",
+    description="Python Implementation of Rigorous Coupled Wave Analysis",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/edmundsj/RCWA",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
