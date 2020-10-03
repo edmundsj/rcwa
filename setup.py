9bcf93d5d555c88bd116a0b7699c1e09d50e50a4
edmundsj <jordan.e@berkeley.edu>
2020-10-03 13:31:00 -0700

    manually incremented version number. Need to find a better way to do this

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="RCWATest", # Replace with your own username
-    version="0.0.1",
+    version="0.0.2",
     author="Jordan Edmunds",
     author_email="jordan.e@berkeley.edu",
     description="Python Implementation of Rigorous Coupled Wave Analysis",
