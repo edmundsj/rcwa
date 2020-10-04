11f9f5087f82a9f9c3a2b51283354b0aa1daee1f
edmundsj <jordan.e@berkeley.edu>
2020-10-04 08:00:32 -0700

    deleted requirements.txt, added dependencies to setup.py and additional source files as 'data files'

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ import os
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open("LICENSE.txt", "r") as fh:
+    license = fh.read()
+
 setuptools.setup(
     name="rcwa",
     version="0.1." + str(os.environ['GITHUB_RUN_NUMBER']),
@@ -16,7 +19,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     include_package_data=True,
     package_data={
-        "rcwa": ['source/*.py'],
+        "rcwa": ['source/*.py', 'netlist/*', 'nkData/*.csv', 'docs/*'],
         },
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -24,4 +27,11 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    install_requires=[
+            'numpy>=1.14.5',
+            'matplotlib>=2.2.0',
+            'pandas>=0.24.0',
+            'scipy>=1.2.2',
+        ]
+    license=license,
 )
