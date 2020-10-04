aaa4665fe7d65c416a798fae623a6e4590646df5
edmundsj <jordan.e@berkeley.edu>
2020-10-04 12:43:58 -0700

    Found the issue. Reflected and transmitted power is not properly normalized when the incident region is not free space. We need to normalize by (I think) the refractive index of the reflecting layer

--- rcwa/examples/bragg_mirror.py
+++ rcwa/examples/bragg_mirror.py
@@ -18,7 +18,9 @@ n2 = 1.45 # refractive index of layer 2 (SiO2)
 t1 = designWavelength/4/n1
 t2 = designWavelength/4/n2
 
-layer0 = rcwa.Layer(n=n1) # Substrate layer
+reflectionLayer = rcwa.Layer(n=1)
+transmissionLayer = rcwa.Layer(n=n1)
+layer0 = rcwa.Layer(n=n1, L=t1)
 layer1 = rcwa.Layer(n=n2, L=t2)
 layer2 = rcwa.Layer(n=n1, L=t1)
 layer3 = rcwa.Layer(n=n2, L=t2)
@@ -29,9 +31,9 @@ layer7 = rcwa.Layer(n=n2, L=t2)
 layer8 = rcwa.Layer(n=n1, L=t1)
 layer9 = rcwa.Layer(n=n2, L=t2)
 layer10 = rcwa.Layer(n=n1, L=t1)
-layer11 = rcwa.Layer(n=1) # top-most layer
-stack = rcwa.LayerStack(layer0, layer1, layer2, layer3, layer4, layer5,
-        layer11)
+stack = rcwa.LayerStack(reflectionLayer,
+       layer0, layer1, layer2, layer3, layer4, layer5, layer6, layer7, layer8, layer9, layer10,
+        transmissionLayer)
 source = rcwa.Source(wavelength=designWavelength)
 
 print("Solving system...")
