a7e34b254f6e77986971fbafff177722ef67ef28
edmundsj <jordan.e@berkeley.edu>
2022-03-19 07:23:27 -0700

    updated README, fixed si ellipsometry example

--- rcwa/examples/si_ellipsometry.py
+++ rcwa/examples/si_ellipsometry.py
@@ -15,13 +15,12 @@ stepWavelength = 0.001
 incident_angle = np.radians(75)
 
 source = Source(wavelength=startWavelength, theta=incident_angle)
-si = Material('Pt')
-bto = Material('Si')
+si = Material('Si')
+breakpoint()
 
 reflectionLayer = Layer(n=1) # Free space
-internal_layer = Layer(material=bto, L=0.1)
 transmissionLayer = Layer(material=si)
-stack = LayerStack(reflectionLayer, internal_layer, transmissionLayer)
+stack = LayerStack(reflectionLayer, transmissionLayer)
 
 print("Solving system...")
 TMMSolver = Solver(stack, source, (1, 1))
@@ -33,17 +32,8 @@ TMMSolver.Solve(wavelengths=wavelengths)
 tan_psi_predicted = np.array([result['tanPsi'] for result in TMMSolver.results])
 cos_delta_predicted = np.array([result['cosDelta'] for result in TMMSolver.results])
 
-measured_data = pd.read_csv(context.nkLocation + '/Pt-ellipsometry-data.csv', delimiter='\t')
-predicted_data = pd.DataFrame({'Wavelength (nm)': wavelengths * 1000, 'Tan(Psi)': tan_psi_predicted, 'Cos(Del)': -cos_delta_predicted})
-
 fig, ax = plt.subplots()
-measured_data.plot(x='Wavelength (nm)', y='Tan(Psi)',ax=ax, fig=fig)
-predicted_data.plot(x='Wavelength (nm)', y='Tan(Psi)', ax=ax, fig=fig)
-ax.legend([r'$Tan(\Psi) Measured$', r'$Tan(\Psi) Predicted$'])
-plt.show()
-
-fig, ax = plt.subplots()
-measured_data.plot(x='Wavelength (nm)', y='Cos(Del)',ax=ax, fig=fig)
-predicted_data.plot(x='Wavelength (nm)', y='Cos(Del)', ax=ax, fig=fig)
-ax.legend([r'$Cos(\Delta) Measured$', r'$Cos(\Delta) Predicted$'])
+ax.plot(wavelengths, tan_psi_predicted)
+ax.plot(wavelengths, cos_delta_predicted)
+ax.legend([r'$Tan(\Psi)$', r'$Cos(\Delta)$'])
 plt.show()
